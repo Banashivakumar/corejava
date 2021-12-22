8695f10152a69fc7d0969398ad63408720ab83d0
Banashivakumar <banaxworkz@gmail.com>
2021-12-22 19:31:58 +0530

    inheritance

---
+++ metal/src/com/xworkz/metal/Iron.java
@@ -0,0 +1,26 @@
+package com.xworkz.metal;
+
+public class Iron extends Metal {
+	
+	public String chemicalName;
+	public float meltingPoint;
+	public String catalyst;
+	public byte automicNumber;
+	public byte electrons;
+
+	public Iron() {
+		System.out.println("Invoking Default Contructor");
+	}
+
+	public Iron(byte electrons,String chemicalName,float meltingPoint,double weight, String color, boolean isductile, double price, float size, String catalyst, byte automicNumber) {
+		super(weight, color, isductile, price, size);
+		this.meltingPoint = meltingPoint;
+		this.chemicalName = chemicalName;
+		this.catalyst = catalyst;
+		this.automicNumber = automicNumber;
+		this.electrons = electrons;
+		System.out.println("Invoking Parameter Contructor");
+	}
+	
+
+}
